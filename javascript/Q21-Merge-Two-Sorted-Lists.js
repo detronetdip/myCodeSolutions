656ce315f84255485419b95b7a5d5a6e765094b3
detronetdip <ayondip2001@gmail.com>
2022-07-09 10:30:00 +0530

    09/07/22

---
+++ javascript/Q21-Merge-Two-Sorted-Lists.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(list1, list2) {
+    var head=new ListNode(0);
+       var l2=head;
+       if(list1==null) return list2;
+       if(list2==null) return list1;
+       while(list1!=null && list2!=null){
+           if(list1.val<list2.val){
+               head.next=list1;
+               list1=list1.next;
+           }else{
+               head.next=list2;
+               list2=list2.next;
+           }
+           head=head.next;
+       }
+       if(list1!=null){
+           head.next=list1;
+       }else if(list2!=null){
+           head.next=list2;
+       }
+       return l2.next;
+};
