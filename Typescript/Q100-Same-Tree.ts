79c87d5d68f31dba6cfbb58d67d8dd85df425ac7
detronetdip <ayondip2001@gmail.com>
2022-07-16 10:56:12 +0530

    16/07/22

---
+++ Typescript/Q100-Same-Tree.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (p == null && q == null) {
+    return true;
+  } else if (p == null || q == null) {
+    return false;
+  }
+  if (p.val != q.val) {
+    return false;
+  } else {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+}
