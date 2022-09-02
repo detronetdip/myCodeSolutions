3a9b1fa873019e0d143404ea0317a958ab00a397
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-02 22:38:46 +0530

    Time: 1 ms (63.09%), Space: 44.1 MB (23.99%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -15,10 +15,10 @@
  */
 class Solution {
 
-    public boolean hasPathSum(TreeNode root, int sum) {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
-        if(root.left == null && root.right == null && sum - root.val == 0) return true;
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        if(root.left == null && root.right == null && targetSum - root.val == 0) return true;
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
         
     }
 }
