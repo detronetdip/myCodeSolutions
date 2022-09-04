567fa9f1607248c87104e0823227010c8e98780a
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-04 18:25:20 +0530

    Time: 2 ms (34.63%), Space: 43.2 MB (27.64%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max=Integer.MIN_VALUE;
+        List<Boolean> ans=new ArrayList<>();
+        for(int i: candies){
+            max=Math.max(max,i);
+        }
+        for(int i: candies){
+            if(i+extraCandies>=max){
+                ans.add(true);
+            }else{
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}
