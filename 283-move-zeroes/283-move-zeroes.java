1d3ea6068b6a1250e99ae2b53317519e192e3089
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-04 18:46:18 +0530

    Time: 72 ms (8.37%), Space: 55.4 MB (9.74%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        for(int i=0;i<nums.length;i++){
+        int n=nums.length;
+        for(int i=0;i<n;i++){
             if(nums[i]==0){
                 int j=i;
-                while(j<nums.length && nums[j]==0 ){
+                while(j<n && nums[j]==0 ){
                     j++;
                 }
-                if(j<nums.length){
+                if(j<n){
                     int t=nums[i];
                 nums[i]=nums[j];
                 nums[j]=t;
