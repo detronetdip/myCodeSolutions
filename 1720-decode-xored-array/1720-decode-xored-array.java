0ff88cb3201b4781ae48a92ad3cfe36e1874128d
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-12 21:32:34 +0530

    Time: 1 ms (100.00%), Space: 44 MB (92.40%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.java
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] decode(int[] encoded, int first) {
-        int[] ans=new int[encoded.length+1];
-        ans[0]=first;
         int e=encoded.length;
+        int[] ans=new int[e+1];
+        ans[0]=first;
         for(int i=1;i<e+1;i++){
             ans[i]=ans[i-1]^encoded[i-1];
         }
