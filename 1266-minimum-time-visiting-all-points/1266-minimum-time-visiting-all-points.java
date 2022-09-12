beede1a1fc8c19372bb1f498a6d3db21577100ec
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-12 22:12:36 +0530

    Time: 4 ms (8.70%), Space: 43.8 MB (39.84%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -1,5 +1,41 @@
 class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
+//         int ans=0;
+//         int length=points.length;
+        
+//         for(int i=0;i<length;i++){
+//             if(i==length-1) break;
+//             int cX=points[i][0];
+//             int cY=points[i][1];
+//             int tX=points[i+1][0];
+//             int tY=points[i+1][1];
+//             int count=0;
+//             while(cX!=tX || cY!=tY){
+//                 if(cX<tX){
+//                     cX++;
+//                 }else if(cX>tX){
+//                     cX--;
+//                 }
+//                 if(cY<tY){
+//                     cY++;
+//                 }else if(cY>tY){
+//                     cY--;
+//                 }
+//                 count++;
+//             }
+//             if(cX<tX){
+//                 cX++;
+//                 count++;
+//             }
+//             if(cY<tY){
+//                 cY++;
+//                 count++;
+//             }
+//             ans+=count;
+//         }
+        
+//         return ans;
+        
         int ans=0;
         int length=points.length;
         
