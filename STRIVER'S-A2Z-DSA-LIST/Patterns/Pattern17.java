66167b9d3e0839b7c6a55f2aefe66150d65f19e9
detronetdip <ayondip2001@gmail.com>
2022-09-12 16:06:28 +0530

    pattern added

---
+++ STRIVER'S-A2Z-DSA-LIST/Patterns/Pattern17.java
@@ -0,0 +1,39 @@
+public class Pattern17 {
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (j <= i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = n; j > 0; j--) {
+                if (j <= i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j <= i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = n; j > 0; j--) {
+                if (j <= i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
