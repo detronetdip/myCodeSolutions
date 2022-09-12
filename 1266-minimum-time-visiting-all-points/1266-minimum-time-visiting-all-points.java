7ed878053a2ec7bbc245b6b44e65d3cc7a5d5f68
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-12 22:11:45 +0530

    Time: 3 ms (10.22%), Space: 43.8 MB (39.84%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -5,33 +5,7 @@ class Solution {
         
         for(int i=0;i<length;i++){
             if(i==length-1) break;
-            int cX=points[i][0];
-            int cY=points[i][1];
-            int tX=points[i+1][0];
-            int tY=points[i+1][1];
-            int count=0;
-            while(cX!=tX || cY!=tY){
-                if(cX<tX){
-                    cX++;
-                }else if(cX>tX){
-                    cX--;
-                }
-                if(cY<tY){
-                    cY++;
-                }else if(cY>tY){
-                    cY--;
-                }
-                count++;
-            }
-            if(cX<tX){
-                cX++;
-                count++;
-            }
-            if(cY<tY){
-                cY++;
-                count++;
-            }
-            ans+=count;
+            ans+=Math.max(Math.abs(points[i][0]-points[i+1][0]),Math.abs(points[i][1]-points[i+1][1]));
         }
         
         return ans;
