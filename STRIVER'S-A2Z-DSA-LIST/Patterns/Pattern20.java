5509cb1d2f68de76bdad8ca5c87c0770c8583f13
detronetdip <ayondip2001@gmail.com>
2022-09-14 09:54:21 +0530

    pattern

---
+++ STRIVER'S-A2Z-DSA-LIST/Patterns/Pattern20.java
@@ -0,0 +1,18 @@
+public class Pattern20 {
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                if(i==1 || i==n-1){
+                    System.out.print("*");
+                }else if(j==1 || j==n-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+       
+    }
+}
