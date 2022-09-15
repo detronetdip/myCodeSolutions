2cb8a605b033c9859e9ac18cf712ffc756f64839
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-15 18:10:54 +0530

    Time: 114 ms (5.25%), Space: 117.3 MB (6.57%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.java
+++ 575-distribute-candies/575-distribute-candies.java
@@ -2,13 +2,15 @@ class Solution {
     public int distributeCandies(int[] candyType) {
         HashMap<Integer,Integer> map=new HashMap<>();
         int l=candyType.length;
+        int s=0;
         for(int i=0;i<l;i++){
             if(!map.containsKey(candyType[i])){
                 map.put(candyType[i],1);
+                s++;
             }else{
                 map.put(candyType[i],map.get(candyType[i])+1);
             }
         }
-        return map.size()<=(l/2)?map.size():l/2;
+        return s<=(l/2)?s:l/2;
     }
 }
