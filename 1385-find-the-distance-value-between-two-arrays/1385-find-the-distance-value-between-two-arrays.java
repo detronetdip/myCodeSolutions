a46b087aa3b0774caa51831ef08d36f788e09843
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-15 17:27:40 +0530

    Time: 5 ms (72.81%), Space: 45 MB (24.70%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
@@ -1,17 +1,25 @@
 class Solution {
+    private static boolean checkRange(int[] arr,int start,int end){
+        int s=0;
+        int e=arr.length-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(arr[m]>= start && arr[m]<=end){
+                return false;
+            }else if(arr[m]<start){
+                s=m+1;
+            }else{
+                e=m-1;
+            }
+        }
+        return true;
+    }
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         int ans=0;
         int m=arr1.length;
-        int n=arr2.length;
+        Arrays.sort(arr2);
         for(int i=0;i<m;i++){
-            int f=1;
-            for(int j=0;j<n;j++){
-                if(Math.abs(arr1[i]-arr2[j])<=d){
-                    f=0;
-                    break;
-                }
-            }
-            if(f==1) ans++;
+            if(checkRange(arr2,arr1[i]-d,arr1[i]+d)) ans++;            
         }
         return ans;
     }
