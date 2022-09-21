066cde69e2dbfa7603fb35229262283ce347466a
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-21 19:09:49 +0530

    Time: 2 ms (71.05%), Space: 50.4 MB (59.14%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=0;
-        for(int i:nums){
-            ans^=i;
+        int ans=0,l=nums.length;
+        for(int i=0;i<l;i++){
+            ans^=nums[i];
         }
         return ans;
     }
