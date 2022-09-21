cd98e630cf683643017ad068f44fa5d98604d77e
Ayondip Jana <64276906+detronetdip@users.noreply.github.com>
2022-09-21 19:03:46 +0530

    Time: 1727 ms (5.01%), Space: 551.7 MB (5.00%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean isPalindrome(String s) {
         String f="";
         String g="";
-        
-        for(int c:s.toLowerCase().toCharArray()){
+        char[] ch=s.toLowerCase().toCharArray();
+        for(int c:ch){
             if((c>=97 && c<=122) ||(c>=48 && c<=57)){
                 f+=(char)c;
                 g=(char)c+g;
